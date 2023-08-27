877de20d2acd5a66e0094124236ec68ec5167308
Arciiix <artur.nowak100@gmail.com>
2023-08-27 19:16:16 +0200

    Add bottom navigation and i18n to the mobile app

---
+++ mobile/lib/components/bottom_navigation.dart
@@ -0,0 +1,33 @@
+import "package:flutter/material.dart";
+import "package:go_router/go_router.dart";
+import "package:handy/gen/strings.g.dart";
+
+class BottomNavigation extends StatelessWidget {
+  const BottomNavigation({
+    required this.navigationShell,
+    Key? key,
+  }) : super(key: key ?? const ValueKey<String>('BottomNavigation'));
+
+  final StatefulNavigationShell navigationShell;
+
+  void onPageChange(int index) {
+    navigationShell.goBranch(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: navigationShell),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: navigationShell.currentIndex,
+        onTap: onPageChange,
+        items: [
+          BottomNavigationBarItem(
+              icon: const Icon(Icons.home), label: t.navigation.home),
+          BottomNavigationBarItem(
+              icon: const Icon(Icons.settings), label: t.navigation.settings)
+        ],
+      ),
+    );
+  }
+}
---
+++ mobile/lib/router.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:handy/components/bottom_navigation.dart';
+import 'package:handy/pages/home_page.dart';
+import 'package:handy/pages/settings_page.dart';
+
+final router = GoRouter(
+  initialLocation: "/",
+  routes: [
+    StatefulShellRoute.indexedStack(
+      builder: (BuildContext context, GoRouterState state,
+          StatefulNavigationShell navigationShell) {
+        return BottomNavigation(
+          navigationShell: navigationShell,
+        );
+      },
+      branches: <StatefulShellBranch>[
+        StatefulShellBranch(
+          routes: <RouteBase>[
+            GoRoute(
+                path: '/',
+                builder: (BuildContext context, GoRouterState state) {
+                  return const HomePage();
+                }),
+          ],
+        ),
+        StatefulShellBranch(routes: [
+          GoRoute(
+            path: '/settings',
+            builder: (BuildContext context, GoRouterState state) {
+              return const SettingsPage();
+            },
+          ),
+        ])
+      ],
+    ),
+  ],
+);
